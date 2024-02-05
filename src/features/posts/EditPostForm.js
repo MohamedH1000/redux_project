02214c990ca45a9e68ec27ecfead7000c574a420
MohamedH1000 <mohammedhisham115@gmail.com>
2024-02-06 02:36:33 +0300

    pharmacy sales

---
+++ src/features/posts/EditPostForm.js
@@ -0,0 +1,134 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { selectPostById, updatePost } from './postsSlice';
+import { useParams, useNavigate } from "react-router-dom";
+
+import { selectAllUsers } from "../users/usersSlice";
+import Select from '@mui/material/Select';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Button from '@mui/material/Button';
+import './addPostForm.css';
+
+const EditPostForm = () => {
+    const { postId } = useParams()
+    const navigate = useNavigate()
+
+    const post = useSelector((state) => selectPostById(state, Number(postId)))
+    const users = useSelector(selectAllUsers)
+
+    const [title, setTitle] = useState(post?.title);
+    const [content, setContent] = useState(post?.body);
+    const [userId, setUserId] = useState(post?.userId);
+    const [requestStatus, setRequestStatus] = useState('idle');
+
+    const dispatch = useDispatch()
+
+    if (!post) {
+        return (
+            <section>
+                <h2>Post not found!</h2>
+            </section>
+        )
+    }
+
+    const onTitleChanged = e => setTitle(e.target.value)
+    const onContentChanged = e => setContent(e.target.value)
+    const onAuthorChanged = e => setUserId(e.target.value)
+
+    const canSave = [title, content, userId].every(Boolean) && requestStatus === 'idle';
+
+    const onSavePostClicked = () => {
+        if (canSave) {
+            try {
+                setRequestStatus('pending')
+                dispatch(updatePost({id: post.id, title, body: content, userId, reactions: post.reactions})).unwrap()
+
+                setTitle('')
+                setContent('')
+                setUserId('')
+                navigate(`/post/${postId}`)
+            } catch (error) {
+                console.error('failed to save the post', error)
+            } finally {
+                setRequestStatus('idle')
+            }
+        }
+    }
+
+    const usersOptions = users.map(user => (
+        <MenuItem key={user.id} value={user.id}>
+            {user.name}
+        </MenuItem>
+    ))
+
+    return (
+        <section>
+            <h2>Edit Post</h2>
+            <form className="form-structure">
+            <label htmlFor="postTitle" >Post Title:</label>
+            <input 
+                type="text"
+                id="postTitle"
+                name="postTitle"
+                value={title}
+                onChange={onTitleChanged}
+                style={{
+                    width:'350px',
+                    margin: '5px',
+                    height:'30px',
+                    padding:'10px'
+                }}
+            />
+            <FormControl fullWidth style={{
+                marginTop:'20px'
+            }}>
+                <InputLabel id="postAuthor" style={{color:'black', fontFamily:'Georgia'}}>Author:</InputLabel>
+                <Select
+                    labelId="postAuthor"
+                    id="postAuthor"
+                    value={userId}
+                    label="Author:"
+                    onChange={onAuthorChanged}
+                    style={{
+                        width:'350px',
+                        margin:'5px',
+                        color:'black',
+                        backgroundColor:'white',
+                        fontFamily:'Georgia'
+                    }}
+                    >
+                    <MenuItem value=""></MenuItem>
+                    {usersOptions}
+                </Select>
+            </FormControl>
+            <label htmlFor="postContent">Content:</label>
+            <textarea 
+                id="postContent"
+                name="postContent"
+                value={content}
+                onChange={onContentChanged}
+                style={{
+                    width:'350px',
+                    margin:'5px',
+                    padding:'10px'
+                }}
+            />
+            <Button 
+                type="button"
+                onClick={onSavePostClicked}
+                variant="contained"
+                style={{margin:'10px 0 20px 0', 
+                        fontFamily:'Georgia', 
+                        textTransform:'none',
+                        width:'350px',
+                    }}
+                disabled={!canSave}
+                >Save Post</Button>
+        </form>
+        </section>
+    )
+}
+
+export default EditPostForm;
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -28,6 +28,16 @@ export const addNewPost = createAsyncThunk('posts/addNewPost', async (initialPos
   }
 })
 
+export const updatePost = createAsyncThunk('posts/updatePost', async (initialPost) => {
+  const { id } = initialPost;
+  try {
+    const response = await axios.put(`${POSTS_URL}/${id}`, initialPost)
+    return response.data
+  } catch (error) {
+    return error.message
+  }
+})
+
 const postSlice = createSlice({
     name: 'posts',
     initialState,
@@ -101,6 +111,17 @@ const postSlice = createSlice({
         }
         console.log(action.payload)
         state.posts.push(action.payload)
+      })
+      .addCase(updatePost.fulfilled, (state, action) => {
+        if (!action.payload?.id) {
+          console.log('update could not complete')
+          console.log(action.payload)
+          return;
+        }
+        const { id } = action.payload;
+        action.payload.data = new Date().toISOString();
+        const posts = state.posts.filter(post => post.id !== id)
+        state.posts = [...posts, action.payload]
       })
     }
 })
